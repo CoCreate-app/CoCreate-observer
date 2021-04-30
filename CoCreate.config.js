95b88b566e44ef1d062ac1c4304511954f61b8af
frank <frank@cocreate.app>
2021-04-30 04:26:09 +0000

    add src

--- CoCreate.config.js
+++ CoCreate.config.js
@@ -10,7 +10,7 @@ module.exports = {
             "path": "./docs/index.html",
             "collection": "files",
             "document_id": "602079ba3c260207bf49cab6",
-            "key": "html",
+            "key": "src",
             "data":{
                 "name": "observer Doc",
                 "domains": ["cocreate.app", "server.cocreate.app", "ws.cocreate.app"],
